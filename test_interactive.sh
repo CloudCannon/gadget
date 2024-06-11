0554b2abe34bd74a5a72c4b79a46d5466c0c2369
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-06-12 11:31:13 +1200

    Add base toolproof setup

---
+++ test_interactive.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+GADGET_CLI=$(realpath "$SCRIPT_DIR/src/cli.js")
+
+npx -y toolproof --placeholders gadget_cli="$GADGET_CLI" --all -i
