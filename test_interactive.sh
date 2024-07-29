9542553529c6131277ff2d0ea10ae486c67576cb
Liam Bigelow <40188355+bglw@users.noreply.github.com>
2024-07-30 11:14:04 +1200

    Rename tests, add vonge test

--- test_interactive.sh
+++ test_interactive.sh
@@ -4,5 +4,6 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
 
 GADGET_DIR=$(realpath "$SCRIPT_DIR/src/")
+TEST_SITES=$(realpath "$SCRIPT_DIR/toolproof_tests/test_sites")
 
-npx -y toolproof --placeholders gadget_dir="$GADGET_DIR" --all -i
+npx -y toolproof --placeholders gadget_dir="$GADGET_DIR" test_sites="$TEST_SITES" --all -i
