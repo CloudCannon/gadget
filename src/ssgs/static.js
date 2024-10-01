50e286408479333a79cd4511be81b2be4b4be48b
Ross Phillips <12723297+rphillips-cc@users.noreply.github.com>
2024-10-02 11:17:19 +1300

    Add output path suggestion for static sites

--- src/ssgs/static.js
+++ src/ssgs/static.js
@@ -27,4 +27,22 @@ export default class Static extends Ssg {
 			}),
 		};
 	}
+
+	/**
+	 * Generates a list of build suggestions.
+	 *
+	 * @param filePaths {string[]} List of input file paths.
+	 * @param options {{ config?: Record<string, any>; source?: string; readFile?: (path: string) => Promise<string | undefined>; }}
+	 * @returns {Promise<import('../types').BuildCommands>}
+	 */
+	async generateBuildCommands(filePaths, options) {
+		const commands = await super.generateBuildCommands(filePaths, options);
+
+		commands.output.unshift({
+			value: '.',
+			attribution: 'common for static sites without build',
+		});
+
+		return commands;
+	}
 }
